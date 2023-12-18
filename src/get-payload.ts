2861216b8581d797c8a15d8b427bef7f74e71cf2
Yanreddddd <johnredkristian.demafeliz@gmail.com>
2023-12-19 07:38:32 +0800

    admin dashboard mongodb success!

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -3,9 +3,8 @@ import path from "path";
 import payload from "payload";
 import type { InitOptions } from "payload/config";
 
-dotenv.config({
-  path: path.resolve(__dirname, "../env"),
-});
+dotenv.config();
+  // {path: path.resolve(__dirname, "./env"),}
 
 let cached = (global as any).payload;
 
@@ -20,29 +19,28 @@ interface Args {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayLoadClient = async ({ initOptions }: Args = {}) => {
+export const getPayloadClient = async ({ initOptions }: Args = {}) => {
   if (!process.env.PAYLOAD_SECRET) {
     throw new Error("PAYLOAD_SECRET is missing");
   }
 
-  if(cached.client) {
-    return cached.client
+  if (cached.client) {
+    return cached.client;
   }
 
-  if(!cached.client) {
+  if (!cached.promise) {
     cached.promise = payload.init({
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
-      ...(initOptions || {})
-    })
+      ...(initOptions || {}),
+    });
   }
 
   try {
-    cached.client = await cached.promise
+    cached.client = await cached.promise;
   } catch (e: unknown) {
-    cached.promise = null
-    throw e
+    cached.promise = null;
+    throw e;
   }
-
-  return cached.client
+  return cached.clienta;
 };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { getPayLoadClient } from "./get-payload";
+import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 const start = async () => {
-  const payload = await getPayLoadClient({
+  const payload = await getPayloadClient({
     initOptions: {
       express: app,
       onInit: async (cms) => {
@@ -18,12 +18,12 @@ const start = async () => {
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
-    payload.logger.info("Next.js started");
+    // payload.logger.info("Next.js started");
 
     app.listen(PORT, async () => {
-      payload.logger.info(
-        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-      );
+      // payload.logger.info(
+      //   `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      // );
     });
   });
 };
